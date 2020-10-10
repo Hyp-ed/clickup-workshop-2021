cc75a2ca91cef505824c78f78dec3cfee89f78aa
celinadongye <celinady25@gmail.com>
2020-10-10 14:06:39 +0200

    Fix compilation error

--- src/demo_person.cpp
+++ src/demo_person.cpp
@@ -11,9 +11,8 @@ int main(){
 //   p.setName("Celina");
 //   p.setAge(20);
 //   p.setHeight(1.70);
-
-//   cout << p.name << "\n";
-//   cout << p.age << "\n";
-//   cout << p.height;
+  cout << p.name_ << "\n";
+  cout << p.age_ << "\n";
+  cout << p.height_;
   return 0;
 }
